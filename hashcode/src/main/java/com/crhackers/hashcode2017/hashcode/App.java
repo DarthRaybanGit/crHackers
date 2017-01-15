57386c70d1bf0316e90d4632b475b3f6a459303c
TheFe91 <alessandrodefendenti@gmail.com>
2017-01-15 16:47:29 +0100

    Initial Commit

---
+++ hashcode/src/main/java/com/crhackers/hashcode2017/hashcode/App.java
@@ -0,0 +1,13 @@
+package com.crhackers.hashcode2017.hashcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
