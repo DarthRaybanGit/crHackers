96dd4d0e6fc0f10b80858d60606028c6d28f0847
TheFe91 <alessandrodefendenti@gmail.com>
2017-01-15 19:10:12 +0100

    working on utility

--- hashcode/src/main/java/com/crhackers/hashcode2017/utility/Utility.java
+++ hashcode/src/main/java/com/crhackers/hashcode2017/utility/Utility.java
@@ -1,11 +1,39 @@
 package com.crhackers.hashcode2017.utility;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utility {
 
 	public static List<Integer[]> utility (int min, int max) {
 		
-		return null;
+		List<Integer[]> result = new ArrayList<Integer[]>();
+		
+		Integer[] utile = {min, 1};
+		result.add(utile);
+		utile[0] = 1;
+		utile[1] = min;
+		result.add(utile);
+		
+		List<Integer> fattori = fattorizza(min);
+		
+		
+		
+		return result;
+	}
+	
+	private static List<Integer> fattorizza(int n) {
+		List<Integer> fattori = new ArrayList<Integer>();
+		int c = 2;
+		while(n > 1) {
+			if (n%c == 0) {
+				fattori.add(c);
+				n = n / c;
+			}
+			else {
+				c++;
+			}
+		}
+		return fattori;
 	}
 }
