ae1e99ad1d0ed9f2f115b54682bbbe164462c45f
TheFe91 <alessandrodefendenti@gmail.com>
2017-01-15 18:01:13 +0100

    added Utility and utility method

--- hashcode/src/main/java/com/crhackers/hashcode2017/utility/Utility.java
+++ hashcode/src/main/java/com/crhackers/hashcode2017/utility/Utility.java
@@ -1,5 +1,11 @@
 package com.crhackers.hashcode2017.utility;
 
+import java.util.List;
+
 public class Utility {
 
+	public static List<Integer> utility (int min, int max) {
+		
+		return null;
+	}
 }
